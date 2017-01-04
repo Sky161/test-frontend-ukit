f21a3e67a96fb4319000855f454f6b06a9b4971c
Andrey Chechkin <andrey.chechkin@me.com>
2017-01-04 12:31:00 +0300

    Init project

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+/**
+* Entry point in app
+*
+* @Author: Andrey Chechkin
+* @Date:   2017-01-03T18:08:28+03:00
+* @Email:  andrey.chechkin@me.com
+* @Last modified by:   Andrey Chechkin
+* @Last modified time: 2017-01-03T18:36:03+03:00
+*/
+
+import styles from "../sass/main.sass";
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+"use strict";
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+	context: __dirname,
+	devtool: "sorce-map",
+	entry: "./src/js/main.js",
+	output: {
+		publicPath: "/",
+		path: "./public",
+		filename: "bundle.js",
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: "babel",
+				query: {
+					presets: ["es2015"]
+				}
+			},
+			{
+				test: /\.sass$/,
+				loader: ExtractTextPlugin.extract(["css?sourceMap", "resolve-url", "sass?sourceMap"])
+			},
+			{
+				test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+				loader: "file",
+				query: {
+					name: "[path][name].[ext]"
+				}
+			}
+		]
+	},
+
+	plugins: [
+		new ExtractTextPlugin("build.css"),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: "commons",
+			filename: "commons.js",
+			minChunks: Infinity
+		})
+	],
+
+	devServer: {
+		contentBase: "./public",
+		inline: true,
+		host: "localhost",
+		port: 3002
+	}
+}
